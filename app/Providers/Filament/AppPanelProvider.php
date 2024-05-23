fdcb3705c0dd53d669df4dd1f5f386431f3137d6
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-05-23 05:42:28 +0500

    Add Notifications

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -38,10 +38,10 @@ class AppPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
+//            ->widgets([
+//                Widgets\AccountWidget::class,
+//                Widgets\FilamentInfoWidget::class,
+//            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
